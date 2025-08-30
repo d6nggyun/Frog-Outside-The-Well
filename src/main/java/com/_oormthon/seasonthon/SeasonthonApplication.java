e68c981400752b6c181460fd2b82c75c2507968a
creatingweb123 <shegegegey1@gmail.com>
2025-08-31 01:36:20 +0900

    first commit

---
+++ src/main/java/com/_oormthon/seasonthon/SeasonthonApplication.java
@@ -0,0 +1,13 @@
+package com._oormthon.seasonthon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeasonthonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeasonthonApplication.class, args);
+	}
+
+}
