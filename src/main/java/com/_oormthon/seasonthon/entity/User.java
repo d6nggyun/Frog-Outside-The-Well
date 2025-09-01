71968bdd2f112ad13a3d7b7a8cb5308335611f33
creatingweb123 <shegegegey1@gmail.com>
2025-09-02 08:45:24 +0900

    feat: add Kakao login functionality

---
+++ src/main/java/com/_oormthon/seasonthon/entity/User.java
@@ -0,0 +1,30 @@
+package com._oormthon.seasonthon.entity;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(unique = true)
+    private Long kakaoId;
+
+    @Column(nullable = true)
+    private Integer age;
+
+    @Builder
+    User(Long kakaoId) {
+        this.name = "익명의 사용자";
+        this.kakaoId = kakaoId;
+    }
+}
