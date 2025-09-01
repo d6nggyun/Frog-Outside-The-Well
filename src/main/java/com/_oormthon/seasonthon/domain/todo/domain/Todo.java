1d9de7968d5b2417cfa391bef495b30996777b4e
d6nggyun <d6nggyun@gmail.com>
2025-09-01 15:29:26 +0900

    docs: 초기 파일 세팅
    # Conflicts:
    #	Dockerfile

--- src/main/java/com/_oormthon/seasonthon/domain/todo/domain/Todo.java
+++ src/main/java/com/_oormthon/seasonthon/domain/todo/domain/Todo.java
@@ -1,4 +1,32 @@
 package com._oormthon.seasonthon.domain.todo.domain;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private Long progress;
+
+    @Column(nullable = false)
+    private LocalDate deadline;
+
+    @Column(nullable = false)
+    private Boolean dailyStarted;
+
+
 }
