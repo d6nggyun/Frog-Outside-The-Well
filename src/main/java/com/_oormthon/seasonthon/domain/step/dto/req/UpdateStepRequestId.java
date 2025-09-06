1fcb9cb865ecffd101791b1eb308f376e0fa4923
d6nggyun <d6nggyun@gmail.com>
2025-09-07 01:49:00 +0900

    fix: Step id 값 NotNull 처리

--- src/main/java/com/_oormthon/seasonthon/domain/step/dto/req/UpdateStepRequestId.java
+++ src/main/java/com/_oormthon/seasonthon/domain/step/dto/req/UpdateStepRequestId.java
@@ -2,11 +2,12 @@ package com._oormthon.seasonthon.domain.step.dto.req;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 @Schema(description = "Step 수정 요청")
 public record UpdateStepRequestId(
 
-        @Schema(description = "step Id", example = "ToDo ERD 설계") @NotBlank(message = "내용이 비어있습니다.") Long stepId,
+        @Schema(description = "step Id", example = "ToDo ERD 설계") @NotNull(message = "내용이 비어있습니다.") Long stepId,
 
         @Schema(description = "내용", example = "ToDo ERD 설계") @NotBlank(message = "내용이 비어있습니다.") String description
 
