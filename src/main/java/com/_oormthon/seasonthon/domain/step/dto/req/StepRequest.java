773c1f8303e966649877abf27590601ab1ead954
creatingweb123 <shegegegey1@gmail.com>
2025-11-09 04:47:24 +0900

    fix: swagger example, annotation

--- src/main/java/com/_oormthon/seasonthon/domain/step/dto/req/StepRequest.java
+++ src/main/java/com/_oormthon/seasonthon/domain/step/dto/req/StepRequest.java
@@ -10,11 +10,11 @@ import java.util.List;
 @Schema(description = "Step 생성 요청")
 public record StepRequest(
 
-                @Schema(description = "날짜", example = "2025-09-02") @NotNull(message = "날짜가 비어있습니다.") LocalDate stepDate,
+        @Schema(description = "날짜", example = "2025-09-02") @NotNull(message = "날짜가 비어있습니다.") LocalDate stepDate,
 
-                @Schema(description = "내용", example = "ToDo ERD 설계") @NotBlank(message = "내용이 비어있습니다.") String description,
+        @Schema(description = "내용", example = "ToDo ERD 설계") @NotBlank(message = "내용이 비어있습니다.") String description,
 
-                @Schema(description = "내용", example = "tips") @NotBlank(message = "내용이 비어있습니다.") List<String> tips
+        @Schema(description = "내용", example = "[\"하루 목표를 명확히 세우기\", \"쉬는 시간엔 스트레칭하기\"]") @NotBlank(message = "내용이 비어있습니다.") List<String> tips
 
 ) {
 }
--- src/main/java/com/_oormthon/seasonthon/domain/todo/dto/req/UpdateTodoDetailRequest.java
+++ src/main/java/com/_oormthon/seasonthon/domain/todo/dto/req/UpdateTodoDetailRequest.java
@@ -4,13 +4,12 @@ import com._oormthon.seasonthon.domain.todo.enums.TodoType;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 @Schema(description = "ToDo 수정 요청")
 public record UpdateTodoDetailRequest(
 
-        @Schema(description = "업무명", example = "우물밖개구리 프로젝트") @NotBlank(message = "업무명이 비어있습니다.") String title,
+                @Schema(description = "업무명", example = "우물밖개구리 프로젝트") @NotBlank(message = "업무명이 비어있습니다.") String title,
 
-        @Schema(description = "ToDo Type", example = "PREVIEW_REVIEW") @NotBlank(message = "ToDo Type이이 비어있습니다.") TodoType todoType
-
-) {
+                @Schema(description = "ToDo Type", example = "PREVIEW_REVIEW") @NotNull(message = "ToDo Type이 비어있습니다.") TodoType todoType) {
 }
